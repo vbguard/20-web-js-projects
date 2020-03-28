2e29d3d8665d29665a2c635e7b9ae950c3436f8f
Viktor Bulvarenko <silken.ua@gmail.com>
2020-03-28 23:36:39 +0200

    custom video playe, exchange currency

---
+++ 03-custom-video-player/script.js
@@ -0,0 +1,64 @@
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// PLAY & PAUSE video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// update play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+//Update progress & timestamp
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  //min
+  let minutes = Math.floor(video.currentTime / 60);
+  if (minutes < 10) {
+    minutes = "0" + String(minutes);
+  }
+
+  //sec
+  let seconds = Math.floor(video.currentTime % 60);
+  if (seconds < 10) {
+    seconds = "0" + String(seconds);
+  }
+
+  timestamp.innerHTML = `${minutes}:${seconds}`;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+function stopVideoStatus() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+// EVENTS
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideoStatus);
+
+progress.addEventListener("click", setVideoProgress);
